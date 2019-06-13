3c74caae4b9ca60c3166278425d05d565dfa8c2d
kaichiro <kcfg23@gmail.com>
2019-06-13 15:32:27 -0300

    Create admin route

---
+++ routes/admin.js
@@ -0,0 +1,28 @@
+const router = require('express').Router()
+
+const Noticia = require('../models/noticia')
+
+router.use((req, res, next) => {
+    if ('user' in req.session) {
+        if (req.session.user.roles.indexOf('admin') >= 0) {
+            return next()
+        } else {
+            res.redirect('/')
+        }
+    }
+    res.redirect('/login')
+})
+
+
+router.get('/', (req, res) => {
+    res.send('admin')
+})
+
+router.get('/noticias', async (req, res) => {
+    const noticias = await Noticia.find({})
+    res.render('noticias/admin', {
+        noticias,
+    })
+})
+
+module.exports = router
