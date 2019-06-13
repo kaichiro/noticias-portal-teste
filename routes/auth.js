024ab06c6e5f2dbabd805d325cc300b2a88d75fb
kaichiro <kcfg23@gmail.com>
2019-06-13 12:43:12 -0300

    Add logout functionality

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,12 @@ router.get('/login', (req, res) => {
     res.render('login')
 })
 
+router.get('/logout', (req, res) => {
+    req.session.destroy(() => {
+        res.redirect('/')
+    })
+})
+
 router.post('/login', async (req, res) => {
     const user = await UserModel.findOne({ username: req.body.username })
     const isValid = await user.checkPassword(req.body.password)
